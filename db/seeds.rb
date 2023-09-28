e923c896568f207efbd5a75e6704ce9d349050e0
BALASUBRAMANIAM S <balasubramaniam.sekar@mallow-tech.com>
2023-09-28 19:08:34 +0530

    Getting ready for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user1 = User.create(email: "bala@gmail.com", password: "bala86..")
-user2 = User.create(email: "bala86@gmail.com", password: "bala86..")
+#user1 = User.create(email: "bala@gmail.com", password: "bala86..")
+#user2 = User.create(email: "bala86@gmail.com", password: "bala86..")
 
-movie1 = Movie.create(name: "Star Wars", release_date: '1978-07-01')
-movie2 = Movie.create(name: "Lord of the Rings", release_date: '2002-03-15')
+#movie1 = Movie.create(name: "Star Wars", release_date: '1978-07-01')
+#movie2 = Movie.create(name: "Lord of the Rings", release_date: '2002-03-15')
 
-#Review.create(user_id: user1.id, movie: movie1, comment: "Great Movie", rating: 5)
-#Review.create(user_id: user2.id, movie: movie1, comment: "Not bad", rating: 3)
-#Review.create(user_id: user1.id, movie: movie2, comment: "Good fantasy story", rating: 4)
+#Review.create(user: user1, movie: movie1, comment: "Great Movie", rating: 5)
+#Review.create(user: user2, movie: movie1, comment: "Not bad", rating: 3)
+#Review.create(user: user1, movie: movie2, comment: "Good fantasy story", rating: 4)
+
+# Find the last four movies created
+movies_to_delete = Movie.order(created_at: :desc).limit(4)
+
+# Delete the selected movies
+movies_to_delete.destroy_all
